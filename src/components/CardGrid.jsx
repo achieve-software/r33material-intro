fe33828d3c8f1ad792eeeda40a0d4b3c84ae2da1
Achieve-software <vtcbcdtyr@gmail.com>
2023-08-11 22:25:11 +0300

    cardz

--- src/components/CardGrid.jsx
+++ src/components/CardGrid.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { Button, CardActionArea, CardActions, Grid } from '@mui/material';
 
-const CardGrid = () => {
+export default function CardGrid() {
   return (
-    <div>CardGrid</div>
-  )
+    <>
+    <Typography
+variant="h4"
+component="h1"
+align="center"
+mt={3}
+color="secondary.light"
+>
+CARD and GRİD LAYOUT
+</Typography>
+
+
+
+<Grid>
+    <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="140"
+          image="/static/images/cards/contemplative-reptile.jpg"
+          alt="green iguana"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Lizard
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Share
+        </Button>
+      </CardActions>
+    </Card>
+    </Grid>
+    </>
+  );
 }
 
-export default CardGrid
+
