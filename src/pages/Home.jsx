47d5931930f8b1a539d7072f9a9aee9e77e6de97
Achieve-software <vtcbcdtyr@gmail.com>
2023-08-12 13:29:57 +0300

    rf

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,8 +2,23 @@ import AppbarComp from "../components/AppbarComp";
 import CardGrid from "../components/CardGrid";
 import TextFieldComp from "../components/TextFieldComp";
 import TypoButtons from "../components/TypoButtons";
+import{createTheme, ThemeProvider} from "@mui/material";
+import {pink, deepOrange } from  "@mui/material/colors";
 
 const Home = () => {
+  const theme = createTheme({
+    palette:{
+      primary:{
+        main:"#000008B"
+      },secondary:{
+        main:pink[500]
+      },
+      error:{
+        main:deepOrange[600]
+      }
+    }
+  })
+  
   return (
     <>
       {/* <TypoButtons />
