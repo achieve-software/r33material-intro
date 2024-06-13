be2eb676546cc3a51f787dff50cd1e9c371c6bfe
Achieve-software <vtcbcdtyr@gmail.com>
2024-06-13 23:28:14 +0300

    FFF

--- src/components/AppbarComp.jsx
+++ src/components/AppbarComp.jsx
@@ -55,7 +55,7 @@ function AppbarComp() {
               textDecoration: "none",
             }}
           >
-            LOGO
+            LOGOS
           </Typography>
 
           <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
